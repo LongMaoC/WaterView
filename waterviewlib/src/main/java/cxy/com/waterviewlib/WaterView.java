8aed018f059d1d88769f6e62dbb17f7b3d33ca2d
chenxingyu <chenxingyu@t-tron.com>
2016-10-13 10:14:59 +0800

    2

--- waterviewlib/src/main/java/cxy/com/waterviewlib/WaterView.java
+++ waterviewlib/src/main/java/cxy/com/waterviewlib/WaterView.java
@@ -8,7 +8,6 @@ import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.Region;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.View;
 
 /*****************************************
